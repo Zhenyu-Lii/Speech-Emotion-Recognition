34785c19293d9640ac767a30695a10f9e0b97639
Zhenyu-Lii <lizhenyu000822@126.com>
2021-12-24 11:02:38 +0800

    debug file path

--- extract_feats/opensmile.py
+++ extract_feats/opensmile.py
@@ -36,10 +36,10 @@ def get_feature_opensmile(config, filepath: str) -> list:
     # single_feature.csv 路径
     single_feat_path = os.path.join(BASE_DIR, config.feature_path, 'single_feature.csv')
     # Opensmile 配置文件路径
-    opensmile_config_path = os.path.join(config.opensmile_path, 'config', config.opensmile_config + '.conf')
+    opensmile_config_path = os.path.join(config.opensmile_path, 'config/is09-13', config.opensmile_config + '.conf')
 
     # Opensmile 命令
-    cmd = 'cd ' + config.opensmile_path + ' && ./SMILExtract -C ' + opensmile_config_path + ' -I ' + filepath + ' -O ' + single_feat_path
+    cmd = 'cd ' + config.opensmile_path + '/bin && ./SMILExtract -C ' + opensmile_config_path + ' -I ' + filepath + ' -O ' + single_feat_path
     print("Opensmile cmd: ", cmd)
     os.system(cmd)
 
